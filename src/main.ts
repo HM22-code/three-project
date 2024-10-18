6bd21d1a98e899b5fd57dcb3fcc1a7aa4ebe2a42
HM22-code <H.M-22@outlook.fr>
2024-10-18 23:14:46 +0200

    Adding Overlay content

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
@@ -23,7 +22,7 @@ function animate() {
 }
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div id="over">
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
@@ -31,13 +30,14 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
     <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div id="container">
+      <button id="start" type="button">Start</button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+function startButton(element: HTMLButtonElement) {
+  element.addEventListener('click', () => console.log("start"))
+}
+
+startButton(document.querySelector<HTMLButtonElement>('#start')!)
